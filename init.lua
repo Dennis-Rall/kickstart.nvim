f9458f176267c9448850ae47639cbc7427c76727
Dennis Rall <dennis.rall@web.de>
2024-12-02 16:22:14 +0100

    feat: disable ruff_lsp in favour for ruff

--- init.lua
+++ init.lua
@@ -676,7 +676,6 @@ require('lazy').setup({
         -- spectral = {},      -- openapi
         pyright = {},
         ruff = {},
-        ruff_lsp = {},
         rust_analyzer = {}, -- rust
         tailwindcss = {},
         terraformls = {},
